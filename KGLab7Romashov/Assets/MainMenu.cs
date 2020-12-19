910d47244bce97c13d2cc160c7c21d522d9a2b04
mloner <shipshon@gmail.com>
2020-12-19 11:35:50 +0400

    first commit

---
+++ KGLab7Romashov/Assets/MainMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+
+public class MainMenu : MonoBehaviour {
+
+    public Slider slider;
+    public struct score {
+        public string datetime;
+        public string scoreTime;
+    };
+    public TextMeshProUGUI LastScoreText;
+    public TextMeshProUGUI Recordtable;
+
+    public void Start() {
+        string scListStr = "";
+        LastScoreText.text = "Последнее время:" + PlayerPrefs.GetString("ScoreTime");
+        List<score> scList = new List<score>();
+        
+        string[] scores = PlayerPrefs.GetString("ScoreTable").Split('\n');
+        foreach(string sc in scores) {
+            if (sc != "")
+            {
+                var tmp = new score();
+                tmp.datetime = sc.Split('|')[0];
+                tmp.scoreTime = sc.Split('|')[1];
+                scList.Add(tmp);
+            }
+            scList = scList.OrderBy(o => o.scoreTime).ToList();
+            
+        }
+        foreach (var el in scList)
+        {
+            scListStr += el.datetime + "|" + el.scoreTime + "\n";
+
+        }
+
+        Recordtable.text = scListStr;
+    }
+    public void PlayGame() {
+        SceneManager.LoadScene("Minigame");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Вышли из игры!");
+        Application.Quit();
+    }
+
+    public void SaveSettings() {
+        PlayerPrefs.SetInt("BallSize", Convert.ToInt32(slider.value));
+    }
+}
---
+++ KGLab7Romashov/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI timeText;
+    public GameObject winTextObject;
+    private Rigidbody rb;
+    private int count = 0;
+    private float movementX;
+    private float movementY;
+
+    static DateTime time;
+    static DateTime lastTime;
+    private bool isPaused = false;
+    private bool gameIsOver = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        setCountText();
+        winTextObject.SetActive(false);
+
+        time = new DateTime(2020,10,10,0,0,0);
+        lastTime = DateTime.Now;
+        float size = PlayerPrefs.GetInt("BallSize");
+        if (size < 100)
+            size = 100;
+        size /= 100;
+
+        rb.transform.localScale = new Vector3(size, size, size);
+        
+    }
+
+    private void Update()
+    {
+        if (!this.isPaused && !this.gameIsOver) {
+            DateTime newDt = DateTime.Now;
+            double tdiff = newDt.Subtract(lastTime).TotalSeconds;
+            if (tdiff > 1)
+            {
+                time = time.AddSeconds(tdiff);
+                lastTime = newDt;
+            }
+            timeText.text = time.ToString("mm:ss");
+        }
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void setCountText()
+    {
+        if(count == 0)
+            countText.text = "Поднимай желтые кубики\nи не трогай красные таблетки!\nCount: " + count.ToString();
+        else if(count > 0)
+            countText.text = "Count: " + count.ToString();
+        if (count >= 5 && !this.gameIsOver)
+        {
+            this.gameIsOver = true;
+            winTextObject.SetActive(true);
+            string str = "[" + DateTime.Now.ToString() + "]|" + time.ToString("mm:ss");
+            PlayerPrefs.SetString("ScoreTime", str);
+            PlayerPrefs.SetString("ScoreTable", PlayerPrefs.GetString("ScoreTable") + "\n" + str);
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+        else if (other.gameObject.CompareTag("PickUpBad"))
+        {
+            other.gameObject.SetActive(false);
+            count--;
+            setCountText();
+        }
+    }
+    
+}
